5e4e2cc448c4403d4542cdc196e4ef2b1b27ce3f
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-15 22:35:46 +0600

    useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="text" ref={inputRef}/>
+      </div>
     </div>
   );
 }
