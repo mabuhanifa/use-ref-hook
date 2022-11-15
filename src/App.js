b8442da29dd6a6183f0d67c03bdbff34a3c8a489
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-15 22:44:57 +0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const inputRef = useRef(null);
+
   useEffect(() => {
     inputRef.current.focus();
   }, []);
+
+  const [names, setNames] = useState([]);
+
+  const onAddName = () => {};
   return (
     <div>
+      <div>
+        {names.map((name) => (
+          <div>{name}</div>
+        ))}
+      </div>
       <div className="main">
         <input type="text" ref={inputRef} />
       </div>
