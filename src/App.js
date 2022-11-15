0f9b0a40e95bdaa7925d4b3ed8b43314d78c0008
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-15 23:40:49 +0600

    id

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import "./App.css";
 
 function App() {
   const inputRef = useRef(null);
-
+const id = useRef(1)
   useEffect(() => {
     inputRef.current.focus();
   }, []);
 
   const [names, setNames] = useState([
-    { id: 1, name: "Jhon Steve" },
-    { id: 2, name: "Abraham Luke" },
+    { id: id.current++, name: "Jhon Steve" },
+    { id: id.current++, name: "Abraham Luke" },
   ]);
 
   const onAddName = () => {
-    setNames([...names, inputRef.current.value]);
+    setNames([...names, { id: id.current++, name: inputRef.current.value }]);
   };
   return (
     <div className="main">
       <div>
         {names.map((name) => (
-          <div key={name.id}>{name.name}</div>
+          <div key={name.id}>{name.id}-{name.name}</div>
         ))}
       </div>
       <div>
