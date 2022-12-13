4995d022d3a9bce97477e129f698f9b5b55df9ee
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-13 09:27:59 +0600

    <CustomRef />

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import Custom from "./Components/Custom";
+import CustomRef from "./Components/CustomRef";
 
 function App() {
-  const inputRef = useRef(null);
-  const id = useRef(1);
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-
-  const [names, setNames] = useState([
-    { id: id.current++, name: "Jhon Steve" },
-    { id: id.current++, name: "Abraham Luke" },
-  ]);
-
-  const onAddName = () => {
-    setNames([...names, { id: id.current++, name: inputRef.current.value }]);
-  };
   return (
-    <div className="main">
-      <div>
-        {names.map((name) => (
-          <div key={name.id}>
-            {name.id}-{name.name}
-          </div>
-        ))}
-      </div>
-      <div>
-        <input type="text" ref={inputRef} />
-      </div>
-      <div>
-        <button onClick={onAddName}>Add Name</button>
-      </div>
-      <div>
-        <div>{JSON.stringify()}</div>
-      </div>
+    <div>
+      <Custom />
+      <CustomRef />
     </div>
   );
 }
--- src/Components/CustomRef.js
+++ src/Components/CustomRef.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
 export default function CustomRef() {
+  const inputRef = useRef(null);
+  const id = useRef(1);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const [names, setNames] = useState([
+    { id: id.current++, name: "Jhon Steve" },
+    { id: id.current++, name: "Abraham Luke" },
+  ]);
+
+  const onAddName = () => {
+    setNames([...names, { id: id.current++, name: inputRef.current.value }]);
+  };
   return (
-    <div>CustomRef</div>
-  )
+    <div className="main">
+      <div>
+        {names.map((name) => (
+          <div key={name.id}>
+            {name.id}-{name.name}
+          </div>
+        ))}
+      </div>
+      <div>
+        <input type="text" ref={inputRef} />
+      </div>
+      <div>
+        <button onClick={onAddName}>Add Name</button>
+      </div>
+      <div>
+        <div>{JSON.stringify()}</div>
+      </div>
+    </div>
+  );
 }
